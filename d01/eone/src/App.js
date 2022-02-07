292d5950950fef7b623fcda9e052e2feb5757a90
devmallett <34248685+devmallett@users.noreply.github.com>
2022-02-07 00:17:01 -0500

    I literally don't know what was so hard about that

--- d01/eone/src/App.js
+++ d01/eone/src/App.js
@@ -1,9 +1,10 @@
 import Form from "./form/Form";
 import Form2 from "./form/Form2";
+import Form3 from "./form/Form3";
 function App() {
   return (
     <>
-      <Form2 />
+      <Form3 />
     </>
   );
 }
